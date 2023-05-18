7653af1b8be68c6d9a5a8a5c9820ede78133467d
laoningmeng <liu529260424@163.com>
2023-05-18 10:48:37 +0800

    增加登录页面

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login-box">
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.login-box {
+    width: 100%;
+    height: 100%;
+    background: url("../assets/bg.png");
+    background-size: cover  ;
+}
+</style>
